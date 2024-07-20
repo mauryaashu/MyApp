92a954d40b7f4d0ffcb385f03b7ef6c2b9a66e3b
ashu <ashu19246@gmail.com>
2024-07-20 19:31:45 +0530

    test1

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SampleApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello");
 		SpringApplication.run(SampleApplication.class, args);
 	}
-
 }
